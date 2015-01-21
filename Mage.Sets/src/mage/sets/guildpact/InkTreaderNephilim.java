7601a360f3486f1cc6051b8cf7dd1ed5332b613a
Duncan Townsend <duncant@mit.edu>
2015-01-20 20:27:06 -0500

    An attempt at making Ink-Treader Nephilim's ability allow the player to choose the order.

--- Mage.Sets/src/mage/sets/guildpact/InkTreaderNephilim.java
+++ Mage.Sets/src/mage/sets/guildpact/InkTreaderNephilim.java
@@ -50,7 +50,18 @@ import mage.game.permanent.Permanent;
 import mage.game.stack.Spell;
 import mage.target.Target;
 import mage.abilities.Modes;
+import mage.cards.Cards;
+import mage.cards.CardsImpl;
+import mage.cards.Card;
 
+import mage.filter.FilterObject;
+import mage.target.TargetObject;
+import mage.MageObject;
+import mage.game.Controllable;
+import java.util.Map;
+import java.util.Set;
+import java.util.HashMap;
+import java.util.HashSet;
 
 /**
  * @author duncancmt
@@ -170,10 +181,11 @@ class InkTreaderNephilimEffect extends OneShotEffect {
     public boolean apply(Game game, Ability source) {
         Spell spell = (Spell) getValue("TriggeringSpell");
         if (spell != null) {
-            for (Permanent permanent : game.getBattlefield().getActivePermanents(filter, source.getControllerId(), source.getSourceId(), game)) {
+            Set<Spell> copies = new HashSet<>();
+            UUID controller = source.getControllerId();
+            for (Permanent permanent : game.getBattlefield().getActivePermanents(filter, controller, source.getSourceId(), game)) {
                 Spell copy = spell.copySpell();                
-                copy.setControllerId(source.getControllerId());
-                copy.setCopiedSpell(true);
+                copy.setControllerId(controller);
                 if (permanent.getId().equals(source.getSourceId())) {
                     continue; // copy only for other creatures
                 }
@@ -200,7 +212,25 @@ class InkTreaderNephilimEffect extends OneShotEffect {
                             }
                         }
                     }
-                    game.getStack().push(copy);
+                    copies.add(copy);
+                    game.getState().setZone(copy.getId(), Zone.PICK);
+                }
+            }
+            TargetPickObject<Spell> target = new TargetPickObject(copies.size(),
+                                                                  new FilterSpell("Copy to put on the stack"),
+                                                                  copies);
+            target.setNotTarget(true);
+            Map<UUID, Spell> copyMap = new HashMap<>();
+            for (Spell s : copies) {
+                copyMap.put(s.getId(), s);
+            }
+            if (target.canChoose(source.getSourceId(), controller, game)) {
+                game.getPlayer(controller).choose(Outcome.Neutral, target, source.getId(), game);
+                for (UUID chosenCopyId : target.getTargets()) {
+                    if (copyMap.containsKey(chosenCopyId)) {
+                        Spell chosenCopy = copyMap.get(chosenCopyId);
+                        chosenCopy.moveToZone(Zone.STACK, source.getSourceId(), game, true);
+                    }
                 }
             }
             return true;
@@ -214,3 +244,104 @@ class InkTreaderNephilimEffect extends OneShotEffect {
     }
 
 }
+
+
+class TargetPickObject<E extends Controllable> extends TargetObject {
+
+    protected FilterObject filter;
+    protected Map<UUID, E> picks;
+
+    // All constructors take a Set picks as an argument because we cannot
+    // enumerate the objects in the "picks" zone. Any object that is not a
+    // member of the Set picks cannot be a target, regardless of its zone.
+    public TargetPickObject(Set<E> picks) {
+        this(1, 1, new FilterObject(""), picks);
+    }
+
+    public TargetPickObject(FilterObject filter, Set<E> picks) {
+        this(1, 1, filter, picks);
+    }
+
+    public TargetPickObject(int numTargets, FilterObject filter, Set<E> picks) {
+        this(numTargets, numTargets, filter, picks);
+    }
+
+    public TargetPickObject(int minNumTargets, int maxNumTargets, FilterObject filter, Set<E> picks) {
+        this.minNumberOfTargets = minNumTargets;
+        this.maxNumberOfTargets = maxNumTargets;
+        this.zone = Zone.PICK;
+        this.filter = filter;
+        this.targetName = filter.getMessage();
+        this.picks = new HashMap<>();
+        for (E pickObject : picks) {
+            this.picks.put(pickObject.getId(), pickObject);
+        }
+    }
+
+    public TargetPickObject(final TargetPickObject target) {
+        super(target);
+        this.filter = target.filter.copy();
+    }
+
+    @Override
+    public FilterObject getFilter() {
+        return filter;
+    }
+
+    @Override
+    public boolean canTarget(UUID id, Ability source, Game game) {
+        if (id != null && picks.containsKey(id) && game.getState().getZone(id).match(zone)) {
+            E pickObject = picks.get(id);
+            return filter.match(pickObject, game);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean canChoose(UUID sourceId, UUID sourceControllerId, Game game) {
+        return canChoose(sourceControllerId, game);
+    }
+
+    @Override
+    public boolean canChoose(UUID sourceControllerId, Game game) {
+        int count = 0;
+        for (UUID pickObjectId : picks.keySet()) {
+            E pickObject = picks.get(pickObjectId);
+            if (game.getPlayer(sourceControllerId).getInRange().contains(pickObject.getControllerId()) &&
+                game.getState().getZone(pickObjectId).match(zone) &&
+                filter.match(pickObject, game)) {
+                count++;
+                if (count >= this.minNumberOfTargets) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Set<UUID> possibleTargets(UUID sourceId, UUID sourceControllerId, Game game) {
+        return possibleTargets(sourceControllerId, game);
+    }
+
+    @Override
+    public Set<UUID> possibleTargets(UUID sourceControllerId, Game game) {
+        Set<UUID> possibleTargets = new HashSet<>();
+        for (UUID pickObjectId : picks.keySet()) {
+            E pickObject = picks.get(pickObjectId);
+            if (game.getPlayer(sourceControllerId).getInRange().contains(pickObject.getControllerId()) &&
+                game.getState().getZone(pickObjectId).match(zone) &&
+                filter.match(pickObject, game)) {
+                possibleTargets.add(pickObject.getId());
+            }
+        }
+        return possibleTargets;
+    }
+
+    @Override
+    public TargetPickObject copy() {
+        return new TargetPickObject(this);
+    }
+
+}
+        
